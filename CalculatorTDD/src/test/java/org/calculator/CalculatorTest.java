8b95238df8143f51a1462b9ebb2b8cf80f8ad709
giteshsawant19 <contact.giteshsawant@gmail.com>
2025-09-25 18:04:14 +0530

    added shouldAddMultipleNumbers Test

--- CalculatorTDD/src/test/java/org/calculator/CalculatorTest.java
+++ CalculatorTDD/src/test/java/org/calculator/CalculatorTest.java
@@ -15,11 +15,19 @@ public class CalculatorTest {
 
     @Test
     public void shouldAddSingleNumber() {
-        assertEquals(1,calculator.Add("1"));
+        assertEquals(1, calculator.Add("1"));
     }
 
     @Test
     public void shouldAddTwoNumbers() {
-        assertEquals(4,calculator.Add("1,3"));
+        assertEquals(4, calculator.Add("1,3"));
+    }
+
+    @Test
+    public void shouldAddMultipleNumbers(){
+        assertEquals(6, calculator.Add("1,2,3"));
+        assertEquals(100, calculator.Add("25,25,25,25"));
+        assertEquals(25, calculator.Add("24,1"));
+        assertEquals(0, calculator.Add("0,0,0"));
     }
 }
