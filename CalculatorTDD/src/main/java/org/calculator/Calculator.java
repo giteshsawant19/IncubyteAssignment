fafc8a38cf02a18148b33453742f11cf9df88bac
giteshsawant19 <contact.giteshsawant@gmail.com>
2025-09-25 17:29:21 +0530

    Initial Commit
    Added shouldReturnZeroOnEmptyString test

---
+++ CalculatorTDD/src/main/java/org/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package org.calculator;
+
+public class Calculator {
+
+    public int Add(String numbers) {
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        return -1;
+    }
+}
---
+++ CalculatorTDD/src/test/java/org/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package org.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void shouldReturnZeroOnEmptyString() {
+        assertEquals(0, calculator.Add(""));
+    }
+}
